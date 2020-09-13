56f29e72dc5b26e3aef1ea932899c779bf8a2f22
Detering0897 <68132491+Detering0897@users.noreply.github.com>
2020-09-13 13:02:07 +0300

    Practic 2

--- src/Ex2/Main.java
+++ src/Ex2/Main.java
@@ -6,28 +6,61 @@ class Dog{
     Dog (String nm, int ag){
         name = nm;
         age = ag;
+        //System.out.println(nm);
+        //System.out.println(ag);
     }
-    void setAge (int ag)
+    public void setAge (int ag)
     {
         age = ag;
     }
-    void setName (String nm)
+    public void setName (String nm)
     {
         name = nm;
     }
-    int ageInHum ()
+    public int ageInHum ()
     {
         return age*7;
     }
-    String toString()
+    public String ToString()
     {
+        String mess="Age of " + name + " is " + Integer.toString(age);
+        return mess;
+    }
+}
 
+class Pitom{
+    Dog pets[] = new Dog[0];
+    public void addNew(int num)
+    {
+        Dog pets2[] = new Dog[pets.length + num];
+        for (int i = 0; i < pets.length;++i)
+        {
+            pets2[i]= pets[i];
+        }
+        for (int i = 0; i < num;++i)
+        {
+            Dog doggy = new Dog("Non",2);
+            pets2[pets.length + i]= doggy;
+        }
+
+        pets = new Dog[pets2.length];
+        for (int i = 0; i < pets2.length;++i)
+        {
+            System.out.println(pets2[i].ToString());
+            pets[i]= pets2[i];
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args)
     {
-
+        Dog bobby = new Dog("Bob",7);
+        bobby.setAge(11);
+        bobby.setName("Old Bob");
+        //System.out.println(bobby.ToString());
+        //System.out.println(bobby.ageInHum());
+        Pitom central = new Pitom();
+        central.addNew(2);
     }
 }
