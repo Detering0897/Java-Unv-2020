c9042bc63d0b90fb5a2b3aac58024381d79c33e5
Detering0897 <68132491+Detering0897@users.noreply.github.com>
2020-10-05 00:23:46 +0300

    ex8

--- src/Ex8/point3/WidgetClass.java
+++ src/Ex8/point3/WidgetClass.java
@@ -1,61 +1,7 @@
 package Ex8.point3;
-
-
-
-import java.awt.*;
 import javax.swing.*;
-
-/*
 import java.awt.*;
-import java.awt.image.*;
-import javax.swing.*;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class WidgetClass extends JFrame {
-
-    ImageIcon icon1 = new ImageIcon("C:\\Users\\nikol\\Pictures\\Screenshots\\Снимок экрана (1).png");
-    ImageIcon icon2 = new ImageIcon("C:\\Users\\nikol\\Pictures\\Screenshots\\12.png");
-    Image im1 = icon1.getImage();
-    Image im2 = icon2.getImage();
-
-    BufferedImage dest;
-
-    float sourcePercentage = 1, destinationPercentage = 0;
-    private static int STEPS = 100;
-    private static float STEP_CHANGE = 1.0f / STEPS;
-    private static int SLEEP_DELAY = 50;
-
-    public WidgetClass() {
-        super("Image Blending");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        dest = new BufferedImage(800, 700, BufferedImage.TYPE_INT_ARGB);
-        setSize(800, 540);
-        TimerTask task = new TimerTask() {
-            public void run() {
-                repaint();
-                sourcePercentage -= STEP_CHANGE;
-                destinationPercentage += STEP_CHANGE;
-            }
-        };
-        Timer timer = new Timer();
-        timer.schedule(task, 0, SLEEP_DELAY);
-    }
-
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        Graphics2D destG = dest.createGraphics();
-        destG.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC,sourcePercentage));
-        destG.drawImage(im1, 0, 0, this);
-        destG.setComposite(AlphaComposite.getInstance(AlphaComposite.XOR,destinationPercentage));
-        destG.drawImage(im2, 0, 0, this);
-        g2d.drawImage(dest, 0, 0, this);
-    }
-
-    public static void main(String args[]) {
-        new WidgetClass().show();
-    }
-}*/
+import java.util.concurrent.TimeUnit;
 
 class Shower extends JPanel
 {
@@ -64,7 +10,7 @@ class Shower extends JPanel
     Shower(String adres)
     {
         image = getToolkit().getImage(adres);
-        setPreferredSize(new Dimension(1290, 820));
+        setPreferredSize(new Dimension(500, 500));
     }
 
     public void paintComponent(Graphics g)
@@ -74,32 +20,44 @@ class Shower extends JPanel
     }
 }
 
-public class WidgetClass extends JFrame {
 
-    WidgetClass(){
-        setContentPane(new Shower("C:\\Users\\Admin\\Downloads\\131e85892a4f50e8b6828d734e2d39ac.jpg"));
+
+class WidgetClass extends JFrame
+{
+    WidgetClass()
+    {
+        setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\1.jpg"));
     }
-    public static void main(String args[]) {
-        WidgetClass myFrame1 = new WidgetClass();
-        myFrame1.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Graphics gr = myFrame1.getGraphics();
-        myFrame1.paintComponents(gr);
-        myFrame1.pack();
-        myFrame1.setVisible(true);
-        WidgetClass myFrame2 = new WidgetClass();
-        myFrame2.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        myFrame2.paintComponents(gr);
-        myFrame2.pack();
-        myFrame2.setVisible(true);
-        WidgetClass myFrame3 = new WidgetClass();
-        myFrame3.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        myFrame3.paintComponents(gr);
-        myFrame3.pack();
-        myFrame3.setVisible(true);
-        WidgetClass myFrame4 = new WidgetClass();
-        myFrame4.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        myFrame4.paintComponents(gr);
-        myFrame4.pack();
-        myFrame4.setVisible(true);
+    public static void main(String[] argv) throws InterruptedException {
+        WidgetClass myFrame = new WidgetClass();
+        myFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Graphics gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        myFrame.pack();
+        myFrame.setVisible(true);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\2.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\3.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\4.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\5.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\6.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
+        TimeUnit.SECONDS.sleep(1);
+        myFrame.setContentPane(new Shower("C:\\Users\\Admin\\Desktop\\PRG\\pic\\7.jpg"));
+        gr = myFrame.getGraphics();
+        myFrame.paintComponents(gr);
     }
 }
